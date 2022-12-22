b6c324dbb0bb181762c63d72e104783e5b97a922
Douglas Fabiano <douglasfabiano23@gmail.com>
2022-12-22 19:23:57 -0300

    ProductDTO with categories

--- src/main/java/com/devsuperior/dscommerce/entities/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/entities/services/ProductService.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscommerce.entities.services;
 
+import com.devsuperior.dscommerce.dto.CategoryDTO;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.dto.ProductMinDTO;
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.entities.services.exceptions.DatabaseException;
 import com.devsuperior.dscommerce.entities.services.exceptions.ResourceNotFoundException;
@@ -75,5 +77,12 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()){
+            Category cat = new Category();
+            cat.setId(catDto.getId());
+            entity.getCategories().add(cat);
+        }
     }
 }
